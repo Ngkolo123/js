83b39400046d8c5b9a1dc79443889a3c18f7472a
Ngkolo123 <44473778+Ngkolo123@users.noreply.github.com>
2018-10-25 23:18:46 +0700

    Update coba2.js

---
+++ coba2.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         BitTube No Offline TIMER
+// @namespace    http://tampermonkey.net/
+// @version      1.0.1
+// @description  BitTube Never Stop Ever Again
+// @author       ReaperBot
+// @updateURL    https://raw.githubusercontent.com/rvina/js/master/ok.js
+// @downloadURL  https://raw.githubusercontent.com/rvina/js/master/ok.js
+// @match        *.bit.tube/*
+// @grant        none
+// ==/UserScript==
+(function () {
+    'use strict';
+    var page
+    var offline
+    var selected
+    var bittube
+    
+
+    var fileme = [
+        'before',
+
+    ];
+    
+    setInterval(mySearch, 5000);
+    
+    function random_item(items) {
+        return items[Math.floor(Math.random() * items.length)];
+    }
+
+    function mySearch() {
+        page = document.body.textContent.toLowerCase() || document.body.innerText.toLowerCase();
+        offline = page.indexOf("offline") !== -1;
+        selected = page.indexOf("selected") !== -1;
+        bittube = page.indexOf("bittube") == -1;
+        if (offline || selected || bittube) {
+            setTimeout(function () {
+                window.location.href = random_item(fileme)
+            }, 3000);
+        }
+    }
+
+    
+
+    function getRndInteger(min, max) {
+        return (Math.floor(Math.random() * (max - min)) + min) * 1000;
+    }
+
+    setInterval(reloadOk, getRndInteger(5760, 7200));
+
+    function reloadOk() {
+        window.location.href = random_item(fileme)
+    }
+
+})();
